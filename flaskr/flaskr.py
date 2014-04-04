e6b16134381cb3cc37f9a2633fc4001bd190ea69
Vladimir Galkin <3budda@gmail.com>
2014-04-05 00:21:49 +0400

    Step 1 and 2

---
+++ flaskr/flaskr.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import os
+import sqlite3
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='password'
+))
+
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+if __name__ == '__main__':
+    app.run()
